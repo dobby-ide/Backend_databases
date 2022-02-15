d64d8a81f46c9bb209b212385f3ea7cae3834758
Fabio Privitera <fabio.privitera@tuni.fi>
2022-02-15 09:33:25 +0200

    task2.sql: modified protection!

--- exercise_5/task2.sql
+++ exercise_5/task2.sql
@@ -12,10 +12,14 @@ GRANT UPDATE ON entry_comment to "commenter_role";
 
 GRANT UPDATE ON comment to "commenter_role";
 
-CREATE USER author_app@localhost;
+CREATE USER author_app@localhost IDENTIFIED BY "veryunsecurepassword";
 
-CREATE USER commenter_app@localhost;
+CREATE USER commenter_app@localhost IDENTIFIED BY "unsecurepassword";
 
 GRANT author_role to author_app@localhost;
 grant commenter_role to commenter_app@localhost;
+
+-- REVOKE ALL PRIVILEGES, GRANT OPTION FROM author_app@localhost;
+-- delete user commenter_app;
+-- drop user author_app@localhost;
 
